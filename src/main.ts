eb80dc35cdbbd3c2e227c91aaa6af62e3d01ff6b
stacktracer <vostroknutov@hotmail.com>
2021-02-23 00:51:20 +0300

    remove token from git

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import "./style";
 
+const url = new URL(location.href);
+
+const authToken = url.searchParams.get('auth_token');
+const authUser = url.searchParams.get('auth_user');
+
+const mdUser = url.searchParams.get('user');
+const mdRepo = url.searchParams.get('repo');
+const mdFile = url.searchParams.get('file');
+
+
 async function rawLoader(path: string) {
   return await fetch(path).then((response) => response.text());
 }
 
 async function ghApi(resource: string, httpMethod: 'POST' | 'GET' = 'GET', body?: any) {
-  const user = "iwnow";
-  const token = "bd085c23abe7c6d84f9d651c25abaa06b22d9057";
   const endpoint = "https://api.github.com";
-  const creds = `${user}:${token}`;
+  const creds = `${authUser}:${authToken}`;
   const auth = btoa(creds);
   const options = {
     mode: "cors",
@@ -30,7 +38,7 @@ async function markdownToHtml(text: string) {
 
 async function run() {
   const testMd = await rawLoader(
-    "https://raw.githubusercontent.com/repapved/test-article/main/test.md"
+    `https://raw.githubusercontent.com/${mdUser}/${mdRepo}/main/${mdFile}`
   );
   const htmlMd = await markdownToHtml(testMd);
 
